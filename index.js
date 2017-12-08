0be4f261832db24b8c7c061d4a28220fb085b4b7
omasakun <masaya2002@outlook.com>
2017-12-08 22:57:29 +0900

    omasakun的侵食
    デザインの一新と音楽リストの読み込み機能をつけました。
    シンプルさを追求せずにワンポイントとしてちゃんと卵の画像を残した。

---
+++ index.js
@@ -0,0 +1,27 @@
+/**
+ *  9:15 開発開始
+ *  9:35 musics.jsの作成完了
+ * 10:08 CSS,HTMLの設計+コーディング完了
+ * 10:28 JSのトリッキーなコード完成
+ * 10:50 デザインの微調節
+ * 実質50行くらいしか無いのに1.5時間もかかってしまった...
+ */
+window.onload = () => Musics.split("\n").filter(v => v !== "").reduce((pv, cv) => {
+	if (cv.startsWith("# ")) {
+		if (pv) document.getElementById("root").appendChild(pv);
+		pv = document.createElement("div");
+		var h = document.createElement("h1");
+		h.innerText = cv.substr(2).trim();
+		pv.appendChild(h);
+		var u = document.createElement("ul");
+		u.setAttribute("class", "songs fullH listV");
+		pv.appendChild(u);
+	} else if (cv.startsWith("- ")) {
+		var l = document.createElement("li");
+		var t = cv.substr(2).split(" ").map(v => v.trim());
+		l.onclick = () => location.href = t[t.length - 1];
+		l.innerText = t.slice(0, t.length - 1).join(" ");
+		pv.getElementsByTagName("ul")[0].appendChild(l);
+	}
+	return pv;
+}, undefined);
---
+++ musics.js
@@ -0,0 +1,41 @@
+/** ファイルの書き方について
+ * 「# 」の後に何か文字を続けることで、それは見出し・タグとなります
+ * 曲は、 「[題名的なsomething] [URL的なSomething]」のようにして、一曲一行で表現します
+ * 空行はテキトーに入れてください。空行は無視して読み込む方針です。
+ * なんかURLが曲名の後ろに付いていると、このファイルが読みにくくて気持ち悪いんだけど・・・ などの苦情はomasakunまで。
+ * 「ひとつの曲にタグをいくつも付けたいんだけど」や「Githubを開かずに曲の追加とかしたいんだけど」、「良いデザイン考えたからデザイン変えてくれ」のような苦情がある場合にも対応する覚悟はできています。それにもかかわらずそれらをまだ改善していないのは、めんどくさい(=不可抗力)+良いデザインの基準が違う、などが原因です。僕は悪くない。
+ */
+Musics = `
+# VOCALOID
+- 命に嫌われている :初音ミク https://www.youtube.com/watch?v=0HYm60Mjm0k
+- 過ぎて行け :msy / 初音ミク https://www.youtube.com/watch?v=wh0HOVL3wC8
+- きみはだいじょうぶ :カルロス袴田 https://www.youtube.com/watch?v=d2yTW64a7Z4
+- 七転び八起きない :MAYU / UtataP https://www.youtube.com/watch?v=dTLM7d74w_A
+- 永遠に幸せになる方法、見つけました。 :Miku / UtataP https://www.youtube.com/watch?v=ijI5Lkatfqk&index=3&list=PLB0tYbJvyKOhkXayc4GOBbyPdJeFOwadF
+- こちら、幸福安心委員会です。 :Miku / UtataP https://www.youtube.com/watch?v=kzUmM-uQVOM&list=PLB0tYbJvyKOhkXayc4GOBbyPdJeFOwadF
+- まさに・・・まさに・・・女神サマ！！ :Miku / UtataP https://www.youtube.com/watch?v=7k85KoUrsMw
+- キミのことが好きでゴメンナサイ :IA / うたたP https://www.youtube.com/watch?v=BeTQMa96_mk
+- ハッピーシンセサイザ :巡音ルカ / GUMI https://www.youtube.com/watch?v=poiZSEjQBgw
+- Happy Synthesizer(ハッピーシンセサイザ English ver.) :巡音ルカ / GUMI https://www.youtube.com/watch?v=VXrptLI1lck
+- 恋の魔法 :ぱなまん / TOKOTOKO（西沢さんP） https://www.youtube.com/watch?v=d0rwL3hLBjs
+- ハロ／ハワユ :初音ミク https://www.youtube.com/watch?v=icFeeQqWAdw
+- 【立体音響】リニューアルBad Apple!! https://www.youtube.com/watch?v=9iUaKmn8xSk
+
+# 東方アレンジs
+- Crazy of Scarlet :魂音泉 ill.bell / ｙｔｒ https://www.youtube.com/watch?v=MHlj_KroeqY
+- THE SECOND COOL :RainyBlueBell https://www.youtube.com/watch?v=T7WfTjkdFuM 
+- 愛き夜道 :ランコ / らっぷびと https://www.youtube.com/watch?v=V6saOsEKLKQ
+- 愛き夜道 :たま / ランコ / 雨天決行 https://www.youtube.com/watch?v=pt_kXItJLYE
+
+# HIPHOP
+- Ante Up :RainyBlueBell https://www.youtube.com/watch?v=QhzEFAM53eQ
+- Rapgloss :RainyBell https://www.youtube.com/watch?v=XsXwDbxjJLg
+
+# DanceMusic
+- 「Chocological」 Original :H△G / Mili https://www.youtube.com/watch?v=hS08QpeIqIE
+- [BGA] MilK [BOFU2015] https://www.youtube.com/watch?v=2CJw_rihu5s
+- Chocological :Mili  https://www.youtube.com/watch?v=7mIUHWR6vIc
+
+# BackGroundMusic(not SOUND)
+- A 440 Hz (Pitch Standard) Tone For Instrument Tuning https://www.youtube.com/watch?v=rFOl-9SNxLY
+`
